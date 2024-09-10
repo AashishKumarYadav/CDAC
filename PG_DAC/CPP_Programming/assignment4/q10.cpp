45bcda2314fd7fa3809aa89fc4472d3c94378a35
Aashish Kumar Yadav <84706612+AashishKumarYadav@users.noreply.github.com>
2024-09-10 13:47:02 +0530

    Update q10.cpp

--- PG_DAC/CPP_Programming/assignment4/q10.cpp
+++ PG_DAC/CPP_Programming/assignment4/q10.cpp
@@ -25,36 +25,4 @@ int main(){
 	return 0;
 }
 
-//output - #include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-
-	int size = 10;
-	int arr[size] = {};
-
-	//cout<<"Enter 10 integers: "<<endl;
-
-	for(int i=0;i<size;i++){
-		cin>>arr[i];
-	}
-
-	for(int i=0;i<size;i++){
-		for(int j=i+1;j<size;j++){
-			if(arr[j]<arr[i]){
-				//swap(arr[i],arr[j]);
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-			}
-		}
-	}
-
-	for(int i=0;i<size;i++){
-		cout<<arr[i]<<" ";
-	}
-
-	return 0;
-}
-
 // output - 0 1 3 3 4 5 7 7 8 9 30 37 67 88 99 (increasing order)
